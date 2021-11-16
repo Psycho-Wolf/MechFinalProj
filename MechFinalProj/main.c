df9f04209dc54a5ea1a57d94247ea00c4919a384
Psycho-Wolf <antony_pavel@hotmail.com>
2021-11-16 09:07:41 -0700

    Solution Init

---
+++ MechFinalProj/main.c
@@ -0,0 +1,18 @@
+/*
+ * MechFinalProj.c
+ *
+ * Created: 11/16/2021 9:06:07 AM
+ * Author : anton
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
