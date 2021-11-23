e8caad768601102b6975b8159a023e9b70ba0bcb
Psycho-Wolf <antony_pavel@hotmail.com>
2021-11-23 12:17:04 -0700

    Update main.c

--- MechFinalProj/main.c
+++ MechFinalProj/main.c
@@ -18,17 +18,29 @@ int main(void){
 	DDRC = 0x0F;
 	
 	uint16_t fwrdDist;
-	
+	// Timer Counter 1 w/ PWM initialization
+	TCCR1A |= (1 << COM2B1);
+	TCCR1B |= (1 << WGM22)  | (1 << WGM20) | (1 << CS21);
+	OCR2A = 300;
+		
 	TCCR1B |= (1 << WGM12) | (1 << ICES1);
 	TCCR1B |= (1 << CS11) | (1 << CS10);	//Start Timer, Prescaler = 64
 	TIMSK1 |= (1 << ICIE1) | (1 << OCIE1A) | (1 << OCIE1B);
-	TCNT1 = 0;
+	TCNT1 = 0;	
 	OCR1A = 14999;
 	OCR1B = 8;
 	sei();	
 	
-    while (1) {		
+    while (1) {
+		
 		fwrdDist = calcFwrdDist();
+		/*if(fwrdDist > 150){
+			motorsOn();
+		}
+		if(fwrdDist <= 150){
+			motorsOff();
+		}*/
+		
     }
 }
 
@@ -53,7 +65,6 @@ ISR(TIMER1_CAPT_vect) {
 		TCCR1B &= (0 << ICES1);
 		TCCR1B |= (1 << CS11) | (1 << CS10);	//Start Timer, Prescaler = 64
 
-
 		} else {
 		endCount = TCNT1;
 		edgeCatch = 1;
@@ -68,3 +79,19 @@ ISR(TIMER1_COMPA_vect){
 ISR(TIMER1_COMPB_vect){
 	PORTB = 0x00;
 }
+
+/*
+void motorsOn()
+{
+	PORTD &= 0xFD;
+	PORTD |= 0x02;
+	CLKlength = 128;
+	OCR1A = CLKlength;
+}
+
+void motorsOff()
+{
+	CLKlength = 0;
+	OCR1A = CLKlength;
+}
+*/
