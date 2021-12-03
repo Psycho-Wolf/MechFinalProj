c99596b2f8ef70ba5377a4805ec2b1fdaea3a474
Psycho-Wolf <antony_pavel@hotmail.com>
2021-12-02 20:11:06 -0700

    Fixed the code

--- MechFinalProj/main.c
+++ MechFinalProj/main.c
@@ -9,56 +9,128 @@
 #include <avr/interrupt.h>
 #include <inttypes.h>
 
-volatile uint16_t endCount, startCount;
+// Global variables
+volatile uint16_t startCount, endCount;		// US echo start and stop
+volatile uint8_t switchIR;					// switch flag for the left and right IR sensors
+volatile uint16_t rDist, lDist;				// left and right distances from respective IR sensors
 
-int calcFwrdDist();
+// Function definitions
+void motorsOn();
+void motorsOff();
+void leftTurn();
+void rightTurn();
+int calcDist();
+int checkDist(uint16_t fDist);
 
-int main(void){
-	DDRB = 0x02;
-	DDRC = 0x0F;
-	
-	uint16_t fwrdDist;
-	// Timer Counter 1 w/ PWM initialization
-	TCCR2A |= (1 << COM2A1);
-	TCCR2B |= (1 << WGM21)  | (1 << WGM20) | (1 << CS21);
-			
-	TCCR1B |= (1 << WGM12) | (1 << ICES1);
+int main(void) {
+
+	// Port inits
+	DDRB = 0x0F;
+	DDRD = 0x0F;
+
+	// Timer 1 configured for US sensor
+	TCCR1B |= (1 << WGM12);
+	TCCR1B |= (1 << ICES1);
+	TCNT1 = 0;
 	TCCR1B |= (1 << CS11) | (1 << CS10);	//Start Timer, Prescaler = 64
 	TIMSK1 |= (1 << ICIE1) | (1 << OCIE1A) | (1 << OCIE1B);
-	TCNT1 = 0;	
 	OCR1A = 14999;
 	OCR1B = 8;
-	sei();	
+
+	// Timer 2 configured for motors & Phase correct PWM
+	TCNT2 = 0;
+	TCCR2A |= (1<<COM2A1);				//Clear on Compare Upcount
+	TCCR2A |= (1<<WGM20);				//PWM w/ Phase Correct
+	TCCR2B |= (1<<CS22) | (1<<CS21) | (1<<CS20);				//No Prescaler on Timer
+	TIMSK2 = (1 << OCIE2A);
 	
-    while (1) {
-		
-		OCR2A = 100;
-		fwrdDist = calcFwrdDist();
-		/*if(fwrdDist > 150){
-			motorsOn();
+	sei();
+	
+	// Variables
+	uint8_t turnNum = 0;
+	uint16_t fDist;
+	
+	while (1) {
+		fDist = calcDist();
+		if(turnNum < 8){				// First Loop run
+			if(checkDist(fDist)){
+				motorsOn();
+				} else if(turnNum == 5){	// First left turn
+				motorsOff();
+				leftTurn();
+				turnNum++;
+				motorsOff();
+				} else {			// All other turns are right
+				motorsOff();
+				rightTurn();
+				motorsOff();
+				turnNum++;
+			}
+			} else if (turnNum < 15){		// Second loop run
+			if(checkDist(fDist)){
+				motorsOn();
+				} else if(turnNum == 13){ // Second left turn CHECK TURN NUMBER
+				motorsOff();
+				leftTurn();
+				turnNum++;
+				motorsOff();
+				} else {			// All other turns are right
+				motorsOff();
+				rightTurn();
+				motorsOff();
+				turnNum++;
+			}
+			} else {
+			// Final corridor
 		}
-		if(fwrdDist <= 150){
-			motorsOff();
-		}*/
 		
-    }
+	}
 }
 
-// US distance calculator
-int calcFwrdDist(){
-	uint16_t count, millimeters = 0;
+// Motor control section
+void motorsOn(){
+	uint16_t pwm = 50;
+	OCR2A = pwm;
+	OCR2B = pwm;
+}
 
+void motorsOff(){
+	OCR2A = 0;
+	OCR2B = 0;
+}
+
+void leftTurn(){
+	uint16_t pwm = 50;
+	OCR2A = pwm;
+	OCR2B = 0;
+}
+
+void rightTurn(){
+	uint16_t pwm = 50;
+	OCR2A = 0;
+	OCR2B = pwm;
+}
+
+// Calculates the forward distance
+int calcDist(){
+	uint16_t count, millimeters;
 	count = endCount - startCount;
 	millimeters = (count >> 8)*176;
-	TCCR1B |= (1 << ICES1);
 	return millimeters;
-	
 }
 
-// US Edge Capture
-ISR(TIMER1_CAPT_vect) {
-	static uint8_t edgeCatch = 1;		//1 => First rising edge
-										//0 => Falling edge
+int checkDist(uint16_t fDist){
+	if(fDist > 200){
+		return 1;
+		} else{
+		return 0;
+	}
+}
+
+// System interrupts
+//Forward distance calculation
+ISR(TIMER1_CAPT_vect){
+	static uint8_t edgeCatch = 1;				//1 => First rising edge
 	if(edgeCatch) {
 		startCount = TCNT1;
 		edgeCatch = 0;
@@ -71,27 +143,31 @@ ISR(TIMER1_CAPT_vect) {
 
 	}
 }
-// US Trigger On
-ISR(TIMER1_COMPA_vect){
+
+// US tigger start
+ISR(TIMER1_COMPA_vect){ // US pulse start
 	PORTB = 0x02;
 }
-// US Trigger On
+
+// US trigger stop
 ISR(TIMER1_COMPB_vect){
 	PORTB = 0x00;
 }
 
-/*
-void motorsOn()
-{
-	PORTD &= 0xFD;
-	PORTD |= 0x02;
-	CLKlength = 128;
-	OCR1A = CLKlength;
+// Empty Timer2 ISR
+ISR(TIMER2_COMPA_vect){
+	
 }
 
-void motorsOff()
-{
-	CLKlength = 0;
-	OCR1A = CLKlength;
+// IR Sensor measure and switch
+ISR(ADC_vect){
+	if(switchIR == 1){
+		rDist = ADC;
+		switchIR = 2;
+		ADMUX |= (1 << MUX0);
+		} else if(switchIR == 2){
+		lDist = ADC;
+		switchIR = 2;
+		ADMUX &= !(1 << MUX0);
+	}
 }
-*/
