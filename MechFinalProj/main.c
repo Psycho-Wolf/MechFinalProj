7f77d7670aa70acda70e5b4955848a88b3825f07
Psycho-Wolf <antony_pavel@hotmail.com>
2021-12-12 16:37:21 -0700

    Commenting

--- MechFinalProj/main.c
+++ MechFinalProj/main.c
@@ -3,6 +3,15 @@
  *
  * Created: 11/16/2021
  * Author : Tony & Max
+ *
+ * This is the final code uploaded to the robot to be used during
+ * the last run on demo day.
+ * 
+ * Pins used:
+ * PB3 & PD3 as pwm for the motors
+ * PB1 & PB0 as trigger and echo pins respectively for the ultrasonis sensor.
+ *
+ * Note: in this code the abbreviation US is used to mean Ultrasonic Sensor
  */ 
 
 #include <avr/io.h>
@@ -11,8 +20,8 @@
 
 // Global variables
 volatile uint16_t startCount, endCount;		// US echo start and stop
-volatile uint8_t switchIR;					// switch flag for the left and right IR sensors
-volatile uint16_t rDist, lDist;				// left and right distances from respective IR sensors
+volatile uint8_t switchIR;					// IR sensor Switch Flag
+volatile uint16_t rDist, lDist;				// Left & Right dist from robot
 volatile uint16_t pwm, turnTimeCnt = 0;
 
 // Function definitions
@@ -44,11 +53,11 @@ int main(void) {
 	TCCR2A |= (1<<COM2B1);				//Clear on Compare Upcount
 	
 	TCCR2A |= (1<<WGM20);				//PWM w/ Phase Correct
-	TCCR2B |= (1<<CS22) | (1<<CS21) | (1<<CS20);				//No Prescaler on Timer
+	TCCR2B |= (1<<CS22) | (1<<CS21) | (1<<CS20);	//No Prescaler on Timer
 	TIMSK2 = (1 << OCIE2A);
 	
 	sei();
-	\
+	
 	// Variables
 	uint8_t turnNum = 0;
 	uint16_t fDist = 0;
@@ -101,6 +110,7 @@ void motorsOff(){
 	OCR2B = 0;
 }
 
+// This code executes a left turn
 void leftTurn(){
 	TIMSK2 = (1 << OCIE2A);
 	pwm = 50;
@@ -113,6 +123,7 @@ void leftTurn(){
 
 }
 
+// This code executes a right turn
 void rightTurn(){
 	TIMSK2 = (1 << OCIE2A);
 	pwm = 50;
@@ -125,14 +136,17 @@ void rightTurn(){
 
 }
 
-// Calculates the forward distance
+// Calculates the distance from front of robot
 int calcDist(){
 	uint16_t count, millimeters;
-	count = endCount - startCount;
+	count = endCount - startCount;		// calculates time length of US
 	millimeters = (count >> 8)*176;
 	return millimeters;
 }
 
+/* This function takes the forward distance as input and outputs a 
+* 1 if it is greater than 200 mm and 0 if less than 200 mm
+*/
 int checkDist(uint16_t fDist){
 	if(fDist > 200){
 		return 1;
@@ -142,17 +156,17 @@ int checkDist(uint16_t fDist){
 }
 
 // System interrupts
+
 //Forward distance calculation
 ISR(TIMER1_CAPT_vect){
 	static uint8_t edgeCatch = 1;				//1 => First rising edge
 	if(edgeCatch) {
-		startCount = TCNT1;
-		edgeCatch = 0;
+		startCount = TCNT1;				// Stores the time of the rising edge
+		edgeCatch = 0;					// Switches between rising and falling edge catch
 		TCCR1B &= (0 << ICES1);
 		TCCR1B |= (1 << CS11) | (1 << CS10);	//Start Timer, Prescaler = 64
-
 		} else {
-		endCount = TCNT1;
+		endCount = TCNT1;						// Stores time of falling edge
 		edgeCatch = 1;
 
 	}
@@ -168,15 +182,16 @@ ISR(TIMER1_COMPB_vect){
 	PORTB &= 0x08;
 }
 
-// Empty Timer2 ISR
+// This isr is used to count the time passed
+// when making a turn
 ISR(TIMER2_COMPA_vect){
 	turnTimeCnt++;
 }
 
 // IR Sensor measure and switch
 ISR(ADC_vect){
-	if(switchIR == 1){
-		rDist = ADC;
+	if(switchIR == 1){ 
+		rDist = ADC;	
 		switchIR = 2;
 		ADMUX |= (1 << MUX0);
 		} else if(switchIR == 2){
